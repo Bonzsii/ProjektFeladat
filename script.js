d0e048d11cb029b7150a257bcec471cb661fe8da
unknown <Bacs Alex@NET1.local>
2024-11-21 13:32:12 +0100

    Számláló

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let cim=document.getElementById("Cim");
-let szamlalo = document.getElementById("szamlalo");
+let timeLeft = 10;
+        const szamlalo = document.getElementById('countdown');
+        const uzenet = document.getElementById('message');
+
+        const interval = setInterval(() => {
+            timeLeft--;
+            szamlalo.textContent = timeLeft;
+
+            if (timeLeft === 0) {
+                clearInterval(interval);
+                szamlalo.style.display = 'none';
+                uzenet.style.display = 'block';
+            }
+        }, 1000); 
