5e7728947a0ad0597dc2c6d06943d76110f07899
unknown <Bacs Alex@NET1.LOCAL>
2024-11-21 13:59:37 +0100

    Új JS

--- script.js
+++ script.js
@@ -13,22 +13,5 @@ let timeLeft = 10;
             }
         }, 1000); 
 
-document.addEventListener('DOMContentLoaded', function() {
 
-    setTimeout(function() {
-               
-        let animMessage = document.createElement("p");
-                
-        animMessage.textContent = "Kezdődik a varázslat!"; 
-           
-        document.body.appendChild(animMessage);
-            animMessage.style.position = "absolute";
-            animMessage.style.top = "50%";
-            animMessage.style.left = "50%";
-            animMessage.style.transform = "translate(-50%, -50%)";
-            animMessage.style.fontSize = "40px";
-            animMessage.style.color = "darkblue"; 
-            animMessage.style.fontWeight = "bold";
-    }, 3000)
-});
         
